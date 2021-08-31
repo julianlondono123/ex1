b8af648be524d2a839b4bf8809c01b63f65b126f
julianlondono123 <julianlondonocol5@gmail.com>
2021-08-31 11:42:21 -0400

    Initial commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
-
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 julian londono
+ */
 public class App 
 {
     public static void main( String[] args )
