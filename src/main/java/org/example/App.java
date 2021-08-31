a968d139aefc2e3d000394cc7caad67395a0a6a5
julianlondono123 <julianlondonocol5@gmail.com>
2021-08-31 10:22:47 -0400

    Initial commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,14 @@
-package org.example;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner input = new Scanner (System.in);
+        System.out.println( "What is your name?" );
+        String name = input.nextLine();
+        System.out.println("Hello " + name);
+
     }
 }
